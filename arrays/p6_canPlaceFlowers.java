40bc558eea59a45a46dbad5a412fc9538a954a63
iris mariah kurien <iriskurien@gmail.com>
2025-02-17 16:48:59 +0530

    add case for n=0

--- arrays/p6_canPlaceFlowers.java
+++ arrays/p6_canPlaceFlowers.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canPlaceFlowers(int[] arr, int n) {
+        if (n==0) return true;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
                 boolean prev = (i == 0) || (arr[i - 1] == 0); 
